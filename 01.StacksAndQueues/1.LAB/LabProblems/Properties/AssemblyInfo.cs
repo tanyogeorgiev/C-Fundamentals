2f247b9b86d9ef1c81d460091b5290cccb68746b
Tanyo Georgiev <tanyogeorgiev@yahoo.com>
2017-05-25 12:12:25 +0300

    Final folder structure

--- 01.StacksAndQueues/1.LAB/LabProblems/Properties/AssemblyInfo.cs
+++ 01.StacksAndQueues/1.LAB/LabProblems/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("01")]
+[assembly: AssemblyTitle("LabProblems")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("01")]
+[assembly: AssemblyProduct("LabProblems")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b1d1b962-80af-4d14-9968-f62b2c4aa2cd")]
+[assembly: Guid("f163ae8e-41f6-49c5-86e7-f68d82bd63a3")]
 
 // Version information for an assembly consists of the following four values:
 //
--- 01.StacksAndQueues/2.EXER/ExerProblems/Properties/AssemblyInfo.cs
+++ 01.StacksAndQueues/2.EXER/ExerProblems/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("01")]
+[assembly: AssemblyTitle("ExerProblems")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("01")]
+[assembly: AssemblyProduct("ExerProblems")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b1a90f62-e293-4814-b939-f58f0138e7c2")]
+[assembly: Guid("6cd65d3d-b766-4ec9-8a8b-ff36acf83a8f")]
 
 // Version information for an assembly consists of the following four values:
 //
